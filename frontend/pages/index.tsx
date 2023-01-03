7eaebe899afd42e7a925372936652036a50d5fb9
Jori <joriguzman@yahoo.com>
2023-01-03 23:38:57 +0800

    Display mid rate; Update amounts based on SGD amount

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -14,47 +14,64 @@ type HomePageProps = {
 
 const inter = Inter({ subsets: ["latin"] });
 
+const MID_RATE_TAG = "MidRate";
+
 export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
   console.log("[Home] getStaticProps()");
   const rates = await getRates();
   const status = await getLatestStatus();
   return {
     props: { rates, status },
-    revalidate: 60 * 60, // 1 hour
+    revalidate: parseInt(process.env.REVALIDATE_SECONDS!),
   };
 };
 
 const Home: NextPage<HomePageProps> = ({ rates, status }) => {
   console.log("[Home] render:", rates);
 
-  const midRateData = rates.find((r) => r.source === "MidRate");
+  const midRateData = rates.find((r) => r.source === MID_RATE_TAG);
   if (midRateData == null) {
     throw new Error("No midrate found");
   }
   const midRate = midRateData.rate;
 
   const initSgd = 500;
-  const initPhp = initSgd * midRate;
-  const [sgdAmount, setSgdAmount] = useState(initSgd.toString());
-  const [phpAmount, setPhpAmount] = useState(initPhp.toFixed(2));
+  const [sgdAmountStr, setSgdAmountStr] = useState(initSgd.toString());
+  const [sgdAmount, setSgdAmount] = useState(initSgd);
+  const [phpAmount, setPhpAmount] = useState(initSgd * midRate);
 
   const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value;
     console.log("newValue", newValue);
     if (/^\d*(\.\d*)?$/.test(newValue)) {
       console.log(`${newValue} is a number`);
-      const sgdAmount = newValue ? parseFloat(newValue) : 0;
-      const phpAmount = sgdAmount * midRate;
-      setSgdAmount(sgdAmount.toString());
-      setPhpAmount(phpAmount.toFixed(2));
+
+      setSgdAmountStr(newValue);
+      const newSgdAmount = newValue ? parseFloat(newValue) : 0;
+      setSgdAmount(newSgdAmount);
+
+      setPhpAmount(newSgdAmount * midRate);
     } else {
       console.log(`"${newValue}" is NOT a number`);
     }
   };
 
-  const ratesRows = rates.map((rate) => {
-    return <RateRow key={rate.id} rateData={rate} />;
+  // Sort by best rate
+  const sortedrates = [...rates].sort((a, b) => {
+    if (a.rate > b.rate) {
+      return -1;
+    }
+    if (a.rate < b.rate) {
+      return 1;
+    }
+    return 0;
   });
+
+  const ratesRows = sortedrates
+    .filter((rate) => rate.source !== MID_RATE_TAG)
+    .map((rate) => {
+      return <RateRow key={rate.id} rateData={rate} amount={sgdAmount} />;
+    });
 
   return (
     <>
@@ -70,6 +87,9 @@ const Home: NextPage<HomePageProps> = ({ rates, status }) => {
       <main>
         <div className="grid place-items-center">
           <h1 className="text-3xl font-bold underline">SGD to PHP rates</h1>
+          <div>
+            <p>MARKET RATE: 1 SGD = {midRate} PHP</p>
+          </div>
           <div className="flex justify-center">
             <div>
               SGD:
@@ -77,14 +97,14 @@ const Home: NextPage<HomePageProps> = ({ rates, status }) => {
               <input
                 type="text"
                 maxLength={8}
-                value={sgdAmount}
+                value={sgdAmountStr}
                 onChange={handleAmountChange}
               />
             </div>
             <div>
               PHP:
               <br />
-              <label>{phpAmount}</label>
+              <label>{phpAmount.toFixed(2)}</label>
             </div>
           </div>
           <div className="p-4">
@@ -120,16 +140,20 @@ const Home: NextPage<HomePageProps> = ({ rates, status }) => {
 
 type RateProps = {
   rateData: ExchangeRate;
+  amount: number;
 };
 
-const RateRow = ({ rateData }: RateProps) => {
+const RateRow = ({ rateData, amount }: RateProps) => {
   console.log("rateData from RateRow", rateData);
+  const exchangeRate = rateData.rate;
+  const fee = rateData.fee;
+  const phpAmount = amount !== 0 ? exchangeRate * (amount - fee) : 0;
   return (
     <tr id={rateData.id}>
       <td>{rateData.source}</td>
-      <td>{rateData.rate.toFixed(2)}</td>
-      <td>{rateData.fee}</td>
-      <td>{0.0}</td>
+      <td>{exchangeRate.toFixed(2)}</td>
+      <td>{fee.toFixed(2)}</td>
+      <td>{phpAmount.toFixed(2)}</td>
     </tr>
   );
 };
